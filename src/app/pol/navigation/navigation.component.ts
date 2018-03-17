b160bccf619cc6b6a916cfe74a0d59d4176662d6
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-03-17 15:05:45 +0100

    Added form to navigation

--- src/app/pol/navigation/navigation.component.ts
+++ src/app/pol/navigation/navigation.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+
+import {} from '@types/googlemaps';
+import TravelMode = google.maps.TravelMode;
+
+import { MapsService } from "../../maps.service";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.min.css']
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.min.css']
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private maps: MapsService) {
+    }
 
+    ngOnInit() {
+        this.form = new FormGroup({
+            from: new FormControl(''),
+            to: new FormControl(''),
+        });
+    }
+
+    submit() {
+        this.maps.navigate(
+            this.form.controls.from.value,
+            this.form.controls.to.value,
+            TravelMode.WALKING,
+        );
+    }
 }
