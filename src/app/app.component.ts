7e5111d7b4ab2ba5f79d5135f740a52907115c0e
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-03-17 13:40:42 +0100

    Added map with navigation functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import TravelMode = google.maps.TravelMode;
+import DirectionsStatus = google.maps.DirectionsStatus;
+import DirectionsRenderer = google.maps.DirectionsRenderer;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    directionsDisplay: DirectionsRenderer;
+    @ViewChild('gmap') gmapElement: any;
+    map: google.maps.Map;
+
+    ngOnInit() {
+        const mapProp = {
+            center: new google.maps.LatLng(51.0543, 3.7174),
+            zoom: 13,
+        };
+        this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+        this.directionsDisplay = new google.maps.DirectionsRenderer({
+            draggable: true,
+            map: this.map,
+        });
+
+        this.navigate('Joremaaie 3', 'Woodrow wilson plein', TravelMode.WALKING);
+    }
+
+    /**
+     * Start navigation from a to b.
+     */
+    navigate(a: string, b: string, mode: TravelMode) {
+        const directionsService = new google.maps.DirectionsService();
+        directionsService.route({
+            origin: a,
+            destination: b,
+            travelMode: mode,
+            provideRouteAlternatives: true,
+        }, (response, status: DirectionsStatus) => {
+            if (status === DirectionsStatus.OK) {
+                const randomRoute = Math.floor(Math.random() * response.routes.length);
+                this.directionsDisplay.setDirections(response);
+                this.directionsDisplay.setRouteIndex(randomRoute);
+            }
+        });
+    }
 }
